6f2a6c4919c3d6d4d804651d63fc58cdfbfa2eb3
nasangmin <sm9171@nate.com>
2017-05-10 15:34:57 +0900

    5.9

--- ioSilsub/src/io/silsub/MyNote.java
+++ ioSilsub/src/io/silsub/MyNote.java
@@ -24,7 +24,7 @@ public class MyNote {
 		sc.nextLine();
 		try {
 			bout = new BufferedWriter(new FileWriter(fileName));
-			bout.write(sb.toString());
+			bout.write(sb.toString()+"\n");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -63,6 +63,53 @@ public class MyNote {
 				fr.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void fileEdit(){
+		BufferedWriter bout = null;
+		StringBuilder sb = new StringBuilder();
+		System.out.print("수정할 파일명 : ");
+		System.out.println();
+		BufferedReader fr = null;
+		String fileName = sc.next();
+
+		char[] readDate = new char[(int) new File(fileName).length()];
+
+		try {
+			fr = new BufferedReader(new FileReader(fileName));
+
+			System.out.println("읽은 값들 확인");
+
+			String str = "";
+			while ((str = fr.readLine()) != null) {
+				System.out.println(str);
+
+			}
+			System.out.println("파일에 추가할 내용을 입력하세요");
+
+			do {
+				str = sc.nextLine();
+				if (str.equals("exit")) {
+					System.out.println("변경할 내용을 추가하시겠습니까? (y/n)");
+					if (sc.next().toUpperCase().equals("Y"))
+						break;
+				}
+				sb.append(str + "\n");
+			} while (true);
+			bout = new BufferedWriter(new FileWriter(fileName, true));
+			bout.write(sb.toString());
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bout.close();
+				fr.close();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
--- ioSilsub/src/io/silsub/TestMyNote.java
+++ ioSilsub/src/io/silsub/TestMyNote.java
@@ -3,13 +3,15 @@ package io.silsub;
 import java.util.Scanner;
 
 public class TestMyNote {
-	Scanner sc=new Scanner(System.in);
+
 	public static void main(String[] args) {
-//		new TestMyNote().menu();
-//		new MyNote().fileSave();
-		new MyNote().fileOpen();
+		new TestMyNote().menu();
+
 	}
 	public static void menu(){
+		MyNote n=new MyNote();
+		Scanner sc=new Scanner(System.in);
+		do{
 		System.out.println("******   MyNote  *******");
 		System.out.println();
 		System.out.println("1. 노트 새로 만들기");
@@ -18,7 +20,13 @@ public class TestMyNote {
 		System.out.println("4. 끝내기");
 		System.out.println();
 		System.out.print("메뉴선택 : ");
-		
+		switch(sc.nextInt()){
+		case 1: n.fileSave(); break;
+		case 2: n.fileOpen(); break;
+		case 3: n.fileEdit(); break;
+		case 4: return;
+		}
+		}while(true);
 	}
 
 }

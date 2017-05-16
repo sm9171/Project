d4453acb7b6490145eec8a156c1608f3c5a2949b
user2 <user2@KH_B>
2017-05-16 21:20:24 +0900

    5.16

--- CarGame/src/CarGame.java
+++ CarGame/src/CarGame.java
@@ -5,6 +5,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 public class CarGame extends JFrame {
+	// ¤·¤©¤·¤©¤·
 	private JLabel label1;
 	private JLabel label2;
 	private JLabel label3;
